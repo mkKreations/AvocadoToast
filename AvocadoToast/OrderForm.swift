64aa1d8981517a609d866db9ce08fdf6c65366de
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-05-31 15:23:35 -0400

    Added UI & functionality to OrderForm

--- AvocadoToast/OrderForm.swift
+++ AvocadoToast/OrderForm.swift
@@ -13,16 +13,45 @@ import SwiftUI
 // ordering our avocado toast
 
 struct OrderForm: View {
+	// this view struct is not the source
+	// of truth for this value
+	@Binding var order: Order
+	
 	var body: some View {
-		Text("OrderForm")
+		VStack {
+			Text("Avocado Toast")
+				.font(.title)
+			
+			Toggle(isOn: $order.includesSalt) {
+				Text("Include Salt")
+			}
+			Toggle(isOn: $order.includesRedPepperFlakes) {
+				Text("Include Red Pepper Flakes")
+			}
+			Stepper(value: $order.quantity) {
+				Text("Quantity: \(order.quantity)")
+			}
+			
+			Button(action: submitOrder) {
+				Text("Order")
+			}
+		}
+		.padding([.leading, .trailing])
+	}
+	
+	private func submitOrder() {
+		print("Order submitted!")
 	}
 }
 
 struct OrderForm_Preview: PreviewProvider {
+	// just some data to pass to present previews
+	static let order = Order(includesSalt: false, includesRedPepperFlakes: true, quantity: 6)
+	
 	static var previews: some View {
 		Group {
-			OrderForm()
-			OrderForm()
+			OrderForm(order: Binding.constant(order))
+			OrderForm(order: Binding.constant(order))
 				.previewDevice("iPhone Xs Max")
 		}
 	}
