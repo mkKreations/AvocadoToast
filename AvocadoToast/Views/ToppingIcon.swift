5f67929327279b5d120d6e5e1d5b0f61e90357f0
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-05-31 18:13:15 -0400

    Added UI logic to ToppingIcon - first draft

--- AvocadoToast/Views/ToppingIcon.swift
+++ AvocadoToast/Views/ToppingIcon.swift
@@ -14,17 +14,45 @@ import SwiftUI
 // all Topping icons will look similar - just
 // slight differences in visual configuration
 
+//struct ToppingShapeIcon: Shape {
+//	func path(in rect: CGRect) -> Path {
+//		let path =
+//	}
+//}
+
 struct ToppingIcon: View {
+	let topping: Topping
+	
+	// configure UI based on Topping
+	// tuple is a good option since we
+	// are only configuring two values
+	private var visualConfig: (backgroundColor: Color, displayText: String) {
+		switch topping {
+		case .salt:
+			return (Color.black, "S")
+		case .redPepperFlakes:
+			return (Color.red, "R")
+		}
+	}
+	
 	var body: some View {
-		Text("ToppingIcon")
+		ZStack {
+			Circle()
+				.foregroundColor(visualConfig.backgroundColor)
+
+			// not sure if the sizing for this is going to work
+			// I want it to fill all available space within circle
+			// need to experiment with it first
+			Text(visualConfig.displayText)
+		}
 	}
 }
 
 struct ToppingIcon_Preview: PreviewProvider {
 	static var previews: some View {
 		Group {
-			ToppingIcon()
-			ToppingIcon()
+			ToppingIcon(topping: Topping.salt)
+			ToppingIcon(topping: Topping.redPepperFlakes)
 				.previewDevice("iPhone Xs Max")
 		}
 	}
