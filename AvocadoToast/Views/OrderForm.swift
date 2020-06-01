1b2d2cba52c6108dd979c8dc948f9a13be14d008
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-05-31 23:42:56 -0400

    Added pickers to OrderForm

--- AvocadoToast/Views/OrderForm.swift
+++ AvocadoToast/Views/OrderForm.swift
@@ -18,8 +18,31 @@ struct OrderForm: View {
 	@Binding var order: Order
 	
 	var body: some View {
-		Form {			
+		Form {
 			Section(header: Text("Avocado Toast").font(.title)) {
+				// I have yet to figure out how to make an enum conform
+				// to identifiable so I can iterate directly over .[allCases]
+				Picker("Bread", selection: $order.bread) {
+					ForEach(0..<Bread.allCases.count) { breadIndex in
+						Text(Bread.allCases[breadIndex].rawValue)
+							.tag(self.order.bread) // make sure to add tag to uniquely id each case
+					}
+				}
+				Picker("Spread", selection: $order.spread) {
+					ForEach(0..<Spread.allCases.count) { spreadIndex in
+						Text(Spread.allCases[spreadIndex].rawValue)
+							.tag(self.order.spread)
+					}
+				}
+				Picker("Avocado", selection: $order.avocado) {
+					ForEach(0..<Avocado.allCases.count) { avocadoIndex in
+						Text(Avocado.allCases[avocadoIndex].rawValue)
+							.tag(self.order.avocado)
+					}
+				}
+			}
+			
+			Section {
 				Toggle(isOn: $order.includesSalt) {
 					Text("Include Salt")
 				}
