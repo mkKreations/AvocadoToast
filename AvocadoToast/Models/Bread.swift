f82030577365c305d32a9c8f2dfbe4fdb16b27a4
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-05-31 23:04:07 -0400

    Added Bread model enum

---
+++ AvocadoToast/Models/Bread.swift
@@ -0,0 +1,21 @@
+//
+//  Bread.swift
+//  AvocadoToast
+//
+//  Created by Marcus on 5/31/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+// this enum will represent different
+// bread options a user can choose for
+// their order
+
+enum Bread {
+	case white
+	case wheat
+	case multi_grain
+	case sourdough
+	case rye
+}
