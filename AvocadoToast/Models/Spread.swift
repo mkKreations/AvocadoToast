5f40bbf60722f4a184c44d5d9c6dbb61aba4a915
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-05-31 23:05:54 -0400

    Added Spread model enum

---
+++ AvocadoToast/Models/Spread.swift
@@ -0,0 +1,24 @@
+//
+//  Spread.swift
+//  AvocadoToast
+//
+//  Created by Marcus on 5/31/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+// this enum will represent different
+// spread options a user can choose for
+// their order
+
+enum Spread {
+	case almond_butter
+	case peanut_butter
+	case honey
+	case almou
+	case tapenade
+	case hummus
+	case vegemite
+	case chutney
+}
