c525d8ac869fbc0fb9c9d14886ee3e02558b1c82
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-05-31 23:13:29 -0400

    Added Avocado model enum

---
+++ AvocadoToast/Models/Avocado.swift
@@ -0,0 +1,18 @@
+//
+//  Avocado.swift
+//  AvocadoToast
+//
+//  Created by Marcus on 5/31/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+// this enum will represent different
+// avocado options a user can choose for
+// their order
+
+enum Avocado {
+	case sliced
+	case mashed
+}
