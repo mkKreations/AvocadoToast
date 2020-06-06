f878ea32aff04233b7630fc5dbfc11e6d3e7eb9b
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-06-05 23:44:34 -0400

    Added basic drag gesture to egg - SEE MORE
    Functionality is still lacking:
    
    - Not allowing user to drag beyond bread
    - Passing in start location
    - Code seems like it needs refactoring
    with the way GeometryReader is being used

--- AvocadoToast/Views/EggPlacementView.swift
+++ AvocadoToast/Views/EggPlacementView.swift
@@ -21,18 +21,41 @@ import SwiftUI
 // 54:82 - container view
 
 struct EggPlacementView: View {
+	@State private var dragLocation = CGPoint.zero
+	
 	var body: some View {
-		ZStack {
-			Image("Rye_Full")
-				.resizable()
-				.scaledToFit()
+		GeometryReader { geo in
+			ZStack {
+				Image("Rye_Full")
+					.resizable()
+					.scaledToFit()
 
-			Image("Egg")
-				.resizable()
-				.aspectRatio(1.0, contentMode: .fit)
-				.frame(width: 250)
+				Image("Egg")
+					.resizable()
+					.aspectRatio(1.0, contentMode: .fit)
+					.frame(width: 250)
+//					.position(CGPoint(x: geo.frame(in: .local).size.width + self.dragLocation.x,
+//														y: geo.frame(in: .local).size.height + self.dragLocation.y))
+					.position(CGPoint(x: (geo.size.width / 2.0) + self.dragLocation.x,
+														y: (geo.size.height  / 2.0) + self.dragLocation.y))
+//					.position(CGPoint(x: self.dragLocation.x,
+//														y: self.dragLocation.y))
+					.gesture(
+						DragGesture()
+							.onChanged {
+								let newLocation = CGPoint(x: $0.location.x - (geo.size.width / 2.0),
+																					y: $0.location.y - (geo.size.height / 2.0))
+								self.dragLocation = newLocation
+							}
+							.onEnded {
+								let newLocation = CGPoint(x: $0.location.x - (geo.size.width / 2.0),
+																					y: $0.location.y - (geo.size.height / 2.0))
+								self.dragLocation = newLocation
+							}
+					)
+			}
+			.padding([.leading, .trailing])
 		}
-		.padding([.leading, .trailing])
 	}
 }
 
