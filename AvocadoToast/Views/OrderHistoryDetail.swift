3d04bfd1d822e2ee130910abf80bb70607e590c1
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-06-09 15:51:23 -0400

    Added OrderHistoryDetail

---
+++ AvocadoToast/Views/OrderHistoryDetail.swift
@@ -0,0 +1,25 @@
+//
+//  OrderHistoryDetail.swift
+//  AvocadoToast
+//
+//  Created by Marcus on 6/9/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderHistoryDetail: View {
+	var body: some View {
+		Text("OrderHistoryDetail")
+	}
+}
+
+struct OrderHistoryDetail_Preview: PreviewProvider {
+	static var previews: some View {
+		Group {
+			OrderHistoryDetail()
+			OrderHistoryDetail()
+				.previewDevice("iPhone Xs Max")
+		}
+	}
+}
